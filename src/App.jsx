f397be1cf38341a0a6aced327f615222aeabcbba
Lorena Avelino <lorenaavelino28@gmail.com>
2024-01-02 12:26:53 -0300

    feat: implementação do Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import Menu from "./components/menu"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className='container'>
+        <Menu />
+      </div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +31,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ src/components/menu/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+const Menu = () => {
+    return (
+        <nav className='menu'>
+            <ul className='itens'>
+                <li><a href="#ocasal">O CASAL</a></li>
+                <li><a href="#cerimonia">CERIMÔNIA</a></li>
+                <li><a href="#lista-presentes">LISTA DE PRESENTES</a></li>
+                <li><a href="#confirme-presença">CONFIRME SUA PRESENÇA</a></li>
+                <li><a href="#recados">RECADOS</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Menu
