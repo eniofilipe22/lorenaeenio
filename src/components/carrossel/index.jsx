83dea1e922019a76babcce863dfc49cd4e15df92
Ênio Filipe <125230002+eniofilipe22@users.noreply.github.com>
2024-02-29 11:58:09 -0300

    adicionando fotinhas carrossel

--- src/components/carrossel/index.jsx
+++ src/components/carrossel/index.jsx
@@ -6,6 +6,13 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Nos from "../../assets/Nos(4).png";
 import Igreja from "../../assets/igreja.png";
 import Capa from "../../assets/capa.webp";
+import Carrossel1 from "../../assets/Carrossel1.webp";
+import Carrossel2 from "../../assets/Carrossel2.webp";
+import Carrossel3 from "../../assets/Carrossel3.webp";
+import Carrossel4 from "../../assets/Carrossel4.webp";
+import Carrossel5 from "../../assets/Carrossel5.webp";
+import Carrossel6 from "../../assets/Carrossel6.webp";
+import Carrossel7 from "../../assets/Carrossel7.webp";
 
 // Import Swiper styles
 import 'swiper/css';
@@ -30,14 +37,27 @@ const Carrossel = () => {
             modules={[Autoplay]}
             >
                 <SwiperSlide>
-                    <img src={Nos} />
+                    <img src={Carrossel1} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={Igreja} />
+                    <img src={Carrossel2} />
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={Capa} />
+                    <img src={Carrossel3} />
                 </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Carrossel4} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Carrossel5} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Carrossel6} />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Carrossel7} />
+                </SwiperSlide>
+                
             </Swiper>
         </div>);
 };
